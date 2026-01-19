cc5d91316492fd3a92f3cc4771b9d5e586c3f723
Ujjawal Kumar <ujjawal1422@gmail.com>
2026-01-19 15:34:09 +0530

    first commit

---
+++ src/main/java/com/tcs/hibernate_relationship_mapping_crud_operation/one_to_one_mapping/controller/InsertPersonAdharController.java
@@ -0,0 +1,5 @@
+package com.tcs.hibernate_relationship_mapping_crud_operation.one_to_one_mapping.controller;
+
+public class InsertPersonAdharController {
+
+}
---
+++ src/main/java/com/tcs/hibernate_relationship_mapping_crud_operation/one_to_one_mapping/dao/PersonAdharDao.java
@@ -0,0 +1,29 @@
+package com.tcs.hibernate_relationship_mapping_crud_operation.one_to_one_mapping.dao;
+
+import com.tcs.hibernate_relationship_mapping_crud_operation.one_to_one_mapping.entity.Adhar;
+import com.tcs.hibernate_relationship_mapping_crud_operation.one_to_one_mapping.entity.Person;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+
+
+public class PersonAdharDao {
+	
+	 EntityManagerFactory emf=Persistence.createEntityManagerFactory("hibernate-mapping");
+	 
+	 EntityManager em= null;
+	 
+	 EntityTransaction et=null;
+	 
+	public Person savePersonAdharDao(Person person, Adhar adhar) {
+		
+      em=emf.createEntityManager();
+      et=em.getTransaction();
+		return null;
+	}
+	
+
+}
